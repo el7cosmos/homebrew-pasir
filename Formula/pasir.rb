9f6fefc249044f11e102e1c89bfe0d7fe3ddd7c5
Abi أب <el@elabee.me>
2025-08-18 19:52:24 +0700

    Add new formula

---
+++ Formula/pasir.rb
@@ -0,0 +1,35 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Pasir < Formula
+  desc "PHP Application Server in Rust"
+  homepage "https://github.com/el7cosmos/pasir"
+  url "https://github.com/el7cosmos/pasir/archive/refs/tags/0.1.0.tar.gz"
+  sha256 "01e28fde4727d2cd2d9af9be2fea1397fe5e4918a0db20842cfacdf5e2d7ea71"
+  license "GPL-3.0"
+
+  depends_on "rust" => :build
+
+  # Additional dependency
+  # resource "" do
+  #   url ""
+  #   sha256 ""
+  # end
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test pasir`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system bin/"program", "do", "something"`.
+    system "false"
+  end
+end
