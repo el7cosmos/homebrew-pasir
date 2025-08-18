5948f23c696b38a1eebe3da55e28413ca5d17ce1
Abi أب <284115+el7cosmos@users.noreply.github.com>
2025-08-18 14:39:18 +0000

    pasir: add 0.1.0 bottle.

--- Formula/pasir.rb
+++ Formula/pasir.rb
@@ -5,6 +5,12 @@ class Pasir < Formula
   sha256 "01e28fde4727d2cd2d9af9be2fea1397fe5e4918a0db20842cfacdf5e2d7ea71"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://ghcr.io/v2/el7cosmos/pasir"
+    sha256 cellar: :any, arm64_sequoia: "e9ccfb622664d0fb1e4a838dd6e5e98ff19e2dafa4959edf84e36dc53342bd70"
+    sha256 cellar: :any, ventura:       "d4b775a82f1b0c6c669a7ac1044c79526e4db488ce8310f66af7db614fb8cca2"
+  end
+
   depends_on "rust" => :build
 
   depends_on "shivammathur/php/php-zts"
