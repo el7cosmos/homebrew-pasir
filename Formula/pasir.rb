7ff2a619334991070a3a938aa52d7ff7aea8795c
Abi أب <el@elabee.me>
2025-08-18 20:05:17 +0700

    Fix audit

--- Formula/pasir.rb
+++ Formula/pasir.rb
@@ -1,12 +1,9 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Pasir < Formula
   desc "PHP Application Server in Rust"
   homepage "https://github.com/el7cosmos/pasir"
   url "https://github.com/el7cosmos/pasir/archive/refs/tags/0.1.0.tar.gz"
   sha256 "01e28fde4727d2cd2d9af9be2fea1397fe5e4918a0db20842cfacdf5e2d7ea71"
-  license "GPL-3.0"
+  license "GPL-3.0-or-later"
 
   depends_on "rust" => :build
 
@@ -21,15 +18,6 @@ class Pasir < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test pasir`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    system "#{bin}/talenta", "--version"
   end
 end
