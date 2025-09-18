996eee43a7f11def4f98450b64a3d4dda8df6668
Abi أب <284115+el7cosmos@users.noreply.github.com>
2025-09-18 02:16:23 +0000

    pasir: update 0.3.1 bottle.

--- Formula/pasir.rb
+++ Formula/pasir.rb
@@ -7,8 +7,8 @@ class Pasir < Formula
 
   bottle do
     root_url "https://ghcr.io/v2/el7cosmos/pasir"
-    sha256 cellar: :any, arm64_sequoia: "c08cc3381c1fb3ce9c1b759913873cae66588659b6cef9b2bc0d9d558c356d98"
-    sha256 cellar: :any, ventura:       "3e068ad53b4c7cde704ea201fe0b69017a403f7c5ec8748259b159f95129ca5f"
+    sha256 cellar: :any, arm64_sequoia: "37704e3f2f59e5b857d4103e2b730e8e2ee11e277660d7ca0460300156f6a185"
+    sha256 cellar: :any, arm64_sonoma:  "0d2f186b970aa871c4549bd5f03b486f75535e51fdea372e50c0342b1c3edda5"
   end
 
   depends_on "rust" => :build
